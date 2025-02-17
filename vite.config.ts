7777a7c3ebcaf7b0338a197efb0adbc13d1d37a9
dosidat1503 <dosidat15031712@gmail.com>
2025-02-17 20:20:12 +0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
   build: {
-    outDir: "./build",
+    outDir: "dist",
+    chunkSizeWarningLimit: 1000,
   },
 });
